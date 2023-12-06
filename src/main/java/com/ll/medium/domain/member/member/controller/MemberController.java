cd8a20b2d0f8a0b18bf52d4322385834bc5af868
tjdus9503 <tjdus03095@gmail.com>
2023-12-06 15:32:15 +0900

    feat: GET /member/join
    MemberController.java 생성
    main.html 생성

---
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -0,0 +1,14 @@
+package com.ll.medium.domain.member.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+    @GetMapping("/join")
+    public String join () {
+        return "/domain/member/member/join";
+    }
+}
