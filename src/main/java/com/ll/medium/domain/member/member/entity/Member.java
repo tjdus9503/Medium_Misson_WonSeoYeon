d4bd9b131351a1d36207e0b27871843a6ff02327
tjdus9503 <tjdus03095@gmail.com>
2023-12-08 16:29:00 +0900

    chore: Member 엔티티에 생성/수정 날짜 추가, JPA Auditing 적용 (값 자동 입력)

--- src/main/java/com/ll/medium/domain/member/member/entity/Member.java
+++ src/main/java/com/ll/medium/domain/member/member/entity/Member.java
@@ -1,10 +1,12 @@
 package com.ll.medium.domain.member.member.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
@@ -16,16 +18,18 @@ import static lombok.AccessLevel.PROTECTED;
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor(access = PROTECTED)
 @EqualsAndHashCode
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
-
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
     @Column(unique = true)
     private String username;
-
     private String password;
-
     private String email;
 }
