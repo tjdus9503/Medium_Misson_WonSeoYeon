733900a740039c5369d8774eebd470d22fbb532f
tjdus9503 <tjdus03095@gmail.com>
2023-12-21 16:24:53 +0900

    refactor: 로그인이 필요한 요청에 대한 예외처리
    - 전역 예외 처리 도입으로 인해, 스프링 시큐리티가 처리하던 예외를 직접 핸들링

--- src/main/java/com/ll/medium/global/exceptionHandler/ControllerExceptionHandler.java
+++ src/main/java/com/ll/medium/global/exceptionHandler/ControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package com.ll.medium.global.exceptionHandler;
 
 import com.ll.medium.global.rq.Rq;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,5 +15,11 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public String handleException(RuntimeException ex) {
         return rq.historyBack(ex.getMessage());
+    }
+
+    // 로그인 필요한 요청에 대한 예외 처리 (스프링 시큐리티가 처리하던 것)
+    @ExceptionHandler(AccessDeniedException.class)
+    public String handleAccessDeniedException(AccessDeniedException ex) {
+        return "redirect:/member/login";
     }
 }
